65e17e99319d7d5fce0471eec4a083b3241b27d7
Sanghyup Lee <slee548@jhu.edu>
2025-08-18 14:51:13 -0400

    removed the target cursor for mobile experience

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import TargetCursor from "@/components/TargetCursor";
 import Content from "@/components/Content";
+import { useState, useEffect } from "react";
 
 import { JetBrains_Mono } from 'next/font/google'
 
@@ -12,9 +13,16 @@ const jetbrainsMono = JetBrains_Mono({
 });
 
 export default function Home() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const ua = navigator.userAgent.toLowerCase();
+    setIsMobile(/iphone|ipad|android|mobile/.test(ua));
+  }, []);
+  
   return (
     <div className={`flex flex-col items-center justify-center min-h-screen ${jetbrainsMono.className}`}>
-      <TargetCursor spinDuration={2} hideDefaultCursor={true} />
+      {!isMobile && <TargetCursor spinDuration={2} hideDefaultCursor={true} />}
       <Content/>
     </div>
   );
