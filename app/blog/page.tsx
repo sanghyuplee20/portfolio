165c243a0a0015a15ac32185d43abc263ecb1439
Sanghyup Lee <slee548@jhu.edu>
2025-08-31 17:51:28 -0400

    added some styles to recipes

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,21 +1,26 @@
-import React from "react";
-import { promises as fs } from 'fs'; 
+import { promises as fs } from "fs";
+import BlogClient from "./BlogClient";
+import { JetBrains_Mono } from "next/font/google";
+
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-jetbrains-mono",
+});
+
 
 export default async function Blog() {
-    const file = await fs.readFile(process.cwd() + '/app/blog/blogs.json', 'utf8');
-    const foodData = JSON.parse(file);
-
-    return (
-        <div>
-            <h1>Blog</h1>
-            <ul>
-                {Object.keys(foodData).map((foodName) => (
-                    <li key={foodName}>
-                        <a href={`/blog/${foodName}`}>{foodData[foodName].name}</a>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+  const file = await fs.readFile(process.cwd() + "/app/blog/blogs.json", "utf8");
+  const foodData = JSON.parse(file);
+  return (
+      <div
+      className={`${jetbrainsMono.className} min-h-screen flex flex-col lg:flex-col bg-white dark:bg-[#313338] transition-colors duration-300 p-8 lg:p-12 gap-8`}
+    >
+      <div className="text-4xl sm:text-5xl lg:text-6xl font-semibold tracking-tight cursor-target text-gray-900 dark:text-white">
+        <h1 className="text-[#f0a04b]">Blog</h1>
+      </div>
+      <BlogClient foodData={foodData} /> 
+      </div>
+      
+      );
 }
-
