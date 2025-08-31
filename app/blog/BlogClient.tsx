2e963692bc7956188124516449c720ffc395b2a2
Sanghyup Lee <slee548@jhu.edu>
2025-08-31 18:06:27 -0400

    added prettier configuration and gif

--- app/blog/BlogClient.tsx
+++ app/blog/BlogClient.tsx
@@ -4,37 +4,39 @@ import { useEffect, useState } from "react";
 import ComesInGoesOutUnderline from "@/components/fancy/text/underline-comes-in-goes-out";
 
 interface FoodData {
-  [key: string]: {
-    name: string;
-  };
+	[key: string]: {
+		name: string;
+	};
 }
 
 export default function BlogClient({ foodData }: { foodData: FoodData }) {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+	const [isDarkMode, setIsDarkMode] = useState(false);
 
-  useEffect(() => {
-    const savedTheme = localStorage.getItem("theme");
-    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    if (savedTheme === "dark" || (!savedTheme && prefersDark)) {
-      setIsDarkMode(true);
-      document.documentElement.classList.add("dark");
-      document.body.style.backgroundColor = "#313338";
-    } else {
-      document.body.style.backgroundColor = "#ffffff";
-    }
-  }, []);
+	useEffect(() => {
+		const savedTheme = localStorage.getItem("theme");
+		const prefersDark = window.matchMedia(
+			"(prefers-color-scheme: dark)"
+		).matches;
+		if (savedTheme === "dark" || (!savedTheme && prefersDark)) {
+			setIsDarkMode(true);
+			document.documentElement.classList.add("dark");
+			document.body.style.backgroundColor = "#313338";
+		} else {
+			document.body.style.backgroundColor = "#ffffff";
+		}
+	}, []);
 
-  return (
-    <ul className="gap-2 text-black dark:text-white text-lg sm:text-l lg:text-xl flex flex-col">
-      {Object.keys(foodData).map((foodName) => (
-        <li key={foodName}>
-          <a href={`/blog/${foodName}`}>
-            <ComesInGoesOutUnderline>
-              {foodData[foodName].name}
-            </ComesInGoesOutUnderline>
-          </a>
-        </li>
-      ))}
-    </ul>
-  );
+	return (
+		<ul className="gap-2 text-black dark:text-white text-lg sm:text-l lg:text-xl flex flex-col">
+			{Object.keys(foodData).map((foodName) => (
+				<li key={foodName}>
+					<a href={`/blog/${foodName}`}>
+						<ComesInGoesOutUnderline>
+							{foodData[foodName].name}
+						</ComesInGoesOutUnderline>
+					</a>
+				</li>
+			))}
+		</ul>
+	);
 }
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,24 +3,37 @@ import BlogClient from "./BlogClient";
 import { JetBrains_Mono } from "next/font/google";
 
 const jetbrainsMono = JetBrains_Mono({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-  variable: "--font-jetbrains-mono",
+	subsets: ["latin"],
+	weight: ["400", "700"],
+	variable: "--font-jetbrains-mono",
 });
 
-
 export default async function Blog() {
-  const file = await fs.readFile(process.cwd() + "/app/blog/blogs.json", "utf8");
-  const foodData = JSON.parse(file);
-  return (
-      <div
-      className={`${jetbrainsMono.className} min-h-screen flex flex-col lg:flex-col bg-white dark:bg-[#313338] transition-colors duration-300 p-8 lg:p-12 gap-8`}
-    >
-      <div className="text-4xl sm:text-5xl lg:text-6xl font-semibold tracking-tight cursor-target text-gray-900 dark:text-white">
-        <h1 className="text-[#f0a04b]">Blog</h1>
-      </div>
-      <BlogClient foodData={foodData} /> 
-      </div>
-      
-      );
+    const file = await fs.readFile(
+		process.cwd() + "/app/blog/blogs.json",
+		"utf8"
+	);
+	const foodData = JSON.parse(file);
+	return (
+		<div
+			className={`${jetbrainsMono.className} min-h-screen flex flex-col lg:flex-col bg-white dark:bg-[#313338] transition-colors duration-300 p-8 lg:p-12 gap-8`}
+		>
+			<div className="text-4xl sm:text-5xl lg:text-6xl font-semibold tracking-tight cursor-target text-gray-900 dark:text-white">
+				<h1 className="text-[#f0a04b]">Blog</h1>
+			</div>
+			<div></div>
+			<video
+				autoPlay
+				loop
+				muted
+				playsInline
+				className="w-64 h-64 rounded-4xl"
+			>
+				<source src="/cat-cooking.webm" type="video/webm" />
+				Your browser does not support the video tag.
+			</video>
+
+			<BlogClient foodData={foodData} />
+		</div>
+	);
 }
